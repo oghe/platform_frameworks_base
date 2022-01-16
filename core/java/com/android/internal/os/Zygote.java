ff98024464be7bec337898892402dbd4fe00a414
Undying-yueyue <orgtech007@gmail.com>
2022-01-16 15:02:04 +0100

    Revert "Zygote: introduce StatusBar Lyrics App Hook"
    * Undying: Deprecated
    
    This reverts commit 527b807740057d6f07fd4d017d35d40298b346de.

--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -35,7 +35,6 @@ import android.system.Os;
 import android.util.Log;
 
 import com.android.internal.net.NetworkUtilsInternal;
-import com.android.internal.util.DeviceHookPropsUtils;
 
 import dalvik.annotation.optimization.CriticalNative;
 import dalvik.annotation.optimization.FastNative;
@@ -884,9 +883,6 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
-
-        // Set Meizu props
-        DeviceHookPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";
